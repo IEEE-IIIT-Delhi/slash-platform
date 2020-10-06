d180600555b87b9defcbe75d1f29fa7c06a5ec98
Mihir Chaturvedi <plibither8@gmail.com>
2020-10-06 11:18:53 +0530

    Fix: Add loading bar

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,12 @@
+<script>
+  import { stores } from "@sapper/app"
+  import PageLoadingBar from "sapper-page-loading-bar/PageLoadingBar.svelte"
+
+  const { preloading } = stores()
+</script>
+
+<PageLoadingBar {preloading} color1="#ff3458d1" color2="#461f26"/>
+
 <slot />
 
 <style lang="scss" global>
