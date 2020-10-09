d4515b4a7c3a40aa04708eb4569a91db70ff03bf
Mihir Chaturvedi <plibither8@gmail.com>
2020-10-09 19:53:44 +0530

    Fix: Couple of changes

--- src/components/play/SidePanel.svelte
+++ src/components/play/SidePanel.svelte
@@ -20,11 +20,25 @@
     background: #111;
     width: 300px;
     display: flex;
+    position: relative;
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
     padding: 7.5vh 0 3vh;
     box-shadow: 5px 0 10px rgba(0,0,0,0.12);
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 100%;
+      top: 0;
+      width: 100%;
+      background-image: url('/paradoxf.png');
+      background-size: cover;
+      background-position: center;
+      opacity: 0.05;
+      pointer-events: none;
+    }
 
     img {
       height: 50px;
