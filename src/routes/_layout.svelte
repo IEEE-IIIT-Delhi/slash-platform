9dc7de20e189d2f61bf11efc10abf68ff0a04509
Mihir Chaturvedi <plibither8@gmail.com>
2020-10-10 13:33:14 +0530

    Fix: analytics for no-admins only

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,8 +5,15 @@
 
   export let segment
 
-  const { preloading } = stores()
+  const { preloading, session } = stores()
 </script>
+
+<svelte:head>
+  {#if !$session.user || !$session.user.admin}
+    <script>window.sa_event = window.sa_event || function () {a = [].slice.call(arguments); sa_event.q ? sa_event.q.push(a) : sa_event.q = [a]};</script>
+    <script data-skip-dnt="true" async defer src="https://sa.mihir.ch/latest.js"></script>
+  {/if}
+</svelte:head>
 
 <PageLoadingBar {preloading} color1="#ff3458d1" color2="#461f26"/>
 
