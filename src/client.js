c1c224a83bba254e8fd4d60b7a0502e0588a21c9
Mihir Chaturvedi <plibither8@gmail.com>
2020-10-21 11:10:55 +0530

    Fix: Remove and unregister service workers

--- src/client.js
+++ src/client.js
@@ -3,3 +3,14 @@ import * as sapper from '@sapper/app'
 sapper.start({
   target: document.body
 })
+
+// Unregister all service workers from client's devices
+// Kuch nahi kar rahe vo apart from caching which I don't
+// really need and the browsers are already doing their bit
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    for (const registration of registrations) {
+      registration.unregister()
+    }
+  })
+}
